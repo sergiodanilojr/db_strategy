a8985106fe03e207203a33b484de59402ccd4ecf
Sergio Danilo Jr <sergiodanilojr@hotmail.com>
2023-04-29 11:18:47 -0300

    fix: implementation of Database connect

--- index.php
+++ index.php
@@ -2,14 +2,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Framework\Core\Database\Database;
-use Framework\Support\Config;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-
-$config = new Config;
-$dbDriver = $config->load('database');
-
-$conn = (new Database('pgsql'))->connect();
+$conn = (new Database())->connect();
 
 dd($conn);
